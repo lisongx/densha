1f0ff3bedbb59ddb0d5748a71552342cbc868423
Li Song <lisong@notimportant.org>
2021-07-29 14:16:31 +0100

    tweak the animation

--- components/LEDText.js
+++ components/LEDText.js
@@ -4,7 +4,9 @@ import styles from '../styles/LEDText.module.css'
 
 function LEDText() {
     return (<div className={styles.container}>
-        <h2 className={styles.ledtext}>Welcome东京艺术家徐月，热烈热烈热烈欢迎</h2>
+        <h2 className={styles.ledtext}>
+            Welcome东京艺术家徐月，热烈热烈1, 非常热烈欢迎热烈2，非常热烈热烈欢迎3。
+        </h2>
     </div>)
 }
 
