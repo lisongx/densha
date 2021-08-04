a1d2bcfeb87058ac2b991b41e6278d0004794c8e
Li Song <lisong@notimportant.org>
2021-08-04 12:05:06 +0100

    video sizing

--- components/Player.js
+++ components/Player.js
@@ -3,35 +3,36 @@ import ReactPlayer from 'react-player'
 
 import styles from '../styles/Player.module.css'
 
-import {
-    useWindowWidth,
-} from '@react-hook/window-size/throttled'
+// import {
+//     useWindowWidth,
+// } from '@react-hook/window-size/throttled'
 
 
 function Player() {
-    const width = useWindowWidth()
-    const artCount = Math.floor(width / 400)
+    // const width = useWindowWidth()
+    const artCount = 3;
     const renderItems = Array.from(Array(artCount).keys())
 
     return (<div className={styles.player}>
         {
-            renderItems.map((item, index) => {
-                return (<ReactPlayer
-                    url='https://www.youtube.com/watch?v=wVeTxfvdLb4'
-                    muted={true}
-                    width={380}
-                    height={400}
-                    key={index}
-                    controls={false}
-                    loop={true}
-                    config={{
-                        youtube: {
-                            playerVars: { modestbranding: 1 }
-                        },
-                    }}
-                    playing={true}
-                />
-                )
+            renderItems.map((_, index) => {
+                return (<div className={styles.player_item}>
+                    <ReactPlayer
+                        url='https://www.youtube.com/watch?v=wVeTxfvdLb4'
+                        muted={true}
+                        width={"100%"}
+                        height={"100%"}
+                        key={index}
+                        controls={false}
+                        loop={true}
+                        config={{
+                            youtube: {
+                                playerVars: { modestbranding: 1 }
+                            },
+                        }}
+                        playing={true}
+                    />
+                </div>)
             })
         }
     </div>)
